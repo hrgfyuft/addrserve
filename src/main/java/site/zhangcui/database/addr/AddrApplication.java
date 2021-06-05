edca082a20342f473fadf9f271ad705bef616892
zhangxin <976645415@qq.com>
2021-06-06 01:46:18 +0800

    init

---
+++ src/main/java/site/zhangcui/database/addr/AddrApplication.java
@@ -0,0 +1,13 @@
+package site.zhangcui.database.addr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AddrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/zhangcui/database/addr/AddrApplicationTests.java
@@ -0,0 +1,13 @@
+package site.zhangcui.database.addr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
